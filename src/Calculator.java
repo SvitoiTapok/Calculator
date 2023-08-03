142007aa76cd85ebc46bb63080179c54f0c5a483
Alexey Pavlov <pavel.alex.030405@gmail.com>
2023-08-03 14:21:22 +0300

    working version

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,51 +5,10 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        File fil = new File("src/input.txt");
-        try {
-            Scanner input = new Scanner(fil);
-            String a[] = input.nextLine().split(" ");
-            try {
-                double first = Double.parseDouble(a[0]);
-                double second = Double.parseDouble(a[2]);
-                if (!(a[1].equals("+")||a[1].equals("-")||a[1].equals("*")||a[1].equals("/"))){
-                    throw new Exception("Operation Error!");
-                }
-                if(a[1].equals("/")&&second==0){
-                    throw new Exception("Error! Division by zero");
-                }
-                System.out.println((a[1].equals("+"))?first+second:(a[1].equals("-"))?first-second:(a[1].equals("*"))?first*second:first/second);
-            }
-            catch (NumberFormatException errorInt){
-                System.out.println("Error! Not number");
-            }
-            catch (Exception b) {
-                System.out.println(b.getMessage());
-            }
-        }
-        catch (FileNotFoundException err){
-            Scanner sc = new Scanner(System.in);
-            String a[] = sc.nextLine().split(" ");
-            try {
-                double first = Double.parseDouble(a[0]);
-                double second = Double.parseDouble(a[2]);
-                if (!(a[1].equals("+")||a[1].equals("-")||a[1].equals("*")||a[1].equals("/"))){
-                    throw new Exception("Operation Error!");
-                }
-                if(a[1].equals("/")&&second==0){
-                    throw new Exception("Error! Division by zero");
-                }
-                System.out.println((a[1].equals("+"))?first+second:(a[1].equals("-"))?first-second:(a[1].equals("*"))?first*second:first/second);
-            }
-            catch (NumberFormatException errorInt){
-                System.out.println("Error! Not number");
-            }
-            catch (Exception b) {
-                System.out.println(b.getMessage());
-            }
-            }
+        User a = new User(new Worker());
+        a.DoCalculate();
     }
-    }
+}
 interface Call{
     public void Callback(int ll);
 }
@@ -79,7 +38,7 @@ class Worker implements Call{
                 System.out.println(b.getMessage());
             }
         }else {
-            File fil = new File("src/input.txt");
+            File fil = new File("input.txt");
             try {
                 Scanner input = new Scanner(fil);
                 String a[] = input.nextLine().split(" ");
