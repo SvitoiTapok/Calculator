9a51375af1300f91ba963e182b049426445c0383
Alexey Pavlov <pavel.alex.030405@gmail.com>
2023-08-03 17:56:00 +0300

    final version

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,53 +18,85 @@ class Worker implements Call{
     @Override
     public void Callback(int ll){
         if (ll == JOptionPane.NO_OPTION){
-            JOptionPane.showMessageDialog(null, "пожалуйста, введите выражение вида \"a <операция> b\" в консоль");
-            Scanner sc = new Scanner(System.in);
-            String a[] = sc.nextLine().split(" ");
-            try {
-                double first = Double.parseDouble(a[0]);
-                double second = Double.parseDouble(a[2]);
-                if (!(a[1].equals("+")||a[1].equals("-")||a[1].equals("*")||a[1].equals("/"))){
-                    throw new Exception("Operation Error!");
+            try (FileWriter file = new FileWriter("output.txt", false)) {
+                JOptionPane.showMessageDialog(null, "пожалуйста, введите выражение вида \"a <операция> b\" в консоль. Чтобы остановить программу, напишите stop");
+                Scanner sc = new Scanner(System.in);
+                String a[] = sc.nextLine().split(" ");
+                while (!a[0].equals("stop")) {
+                    file.write(String.join(" ", a) + " = ");
+                    try {
+                        double first = Double.parseDouble(a[0]);
+                        double second = Double.parseDouble(a[2]);
+                        if (!(a[1].equals("+") || a[1].equals("-") || a[1].equals("*") || a[1].equals("/"))) {
+                            throw new Exception("Operation Error!");
+                        }
+                        if (a[1].equals("/") && second == 0) {
+                            throw new Exception("Error! Division by zero");
+                        }
+                        file.write(Double.toString((a[1].equals("+")) ? first + second : (a[1].equals("-")) ? first - second : (a[1].equals("*")) ? first * second : first / second));
+                    } catch (NumberFormatException errorInt) {
+                        file.write("Error! Not number");
+                    } catch (Exception b) {
+                        file.write(b.getMessage());
+                    }
+                    finally {
+                        a = sc.nextLine().split(" ");
+                        file.write("\n");
+                    }
                 }
-                if(a[1].equals("/")&&second==0){
-                    throw new Exception("Error! Division by zero");
-                }
-                System.out.println((a[1].equals("+"))?first+second:(a[1].equals("-"))?first-second:(a[1].equals("*"))?first*second:first/second);
-            }
-            catch (NumberFormatException errorInt){
-                System.out.println("Error! Not number");
-            }
-            catch (Exception b) {
-                System.out.println(b.getMessage());
+            } catch (IOException b){
+                System.out.println("IOException");
             }
         }else {
             Scanner sc = new Scanner(System.in);
             JOptionPane.showMessageDialog(null, "Пожалуйста, введите путь к файлу в консоль");
             String path = sc.nextLine();
             File fil = new File(path);
-            try {
-                Scanner input = new Scanner(fil);
-                String a[] = input.nextLine().split(" ");
+            try (FileWriter file = new FileWriter("output.txt", false)) {
+
                 try {
-                    double first = Double.parseDouble(a[0]);
-                    double second = Double.parseDouble(a[2]);
-                    if (!(a[1].equals("+") || a[1].equals("-") || a[1].equals("*") || a[1].equals("/"))) {
-                        throw new Exception("Operation Error!");
+
+                    Scanner input = new Scanner(fil);
+                    while (input.hasNextLine()) {
+                        String a[] = input.nextLine().split(" ");
+                        file.write(String.join(" ", a) + " = ");
+                        try {
+                            double first = Double.parseDouble(a[0]);
+                            double second = Double.parseDouble(a[2]);
+                            if (!(a[1].equals("+") || a[1].equals("-") || a[1].equals("*") || a[1].equals("/"))) {
+                                throw new Exception("Operation Error!");
+                            }
+                            if (a[1].equals("/") && second == 0) {
+                                throw new Exception("Error! Division by zero");
+                            }
+                            if (a[1].equals("+")) {
+                                file.write(String.valueOf(first+second));
+                            }
+                            if(a[1].equals("-"))
+                                file.write(Double.toString(first - second));
+                            if(a[1].equals("*"))
+                                file.write(Double.toString(first * second));
+                            if(a[1].equals("/"))
+                                file.write(Double.toString(first / second));
+                        } catch (NumberFormatException errorInt) {
+                            file.write("Error! Not number");
+                        } catch (Exception b) {
+                            String aboa = b.getMessage();
+                            file.write(aboa);
+                        }
+                        finally {
+                            file.write("\n");
+                        }
                     }
-                    if (a[1].equals("/") && second == 0) {
-                        throw new Exception("Error! Division by zero");
+                    } catch(FileNotFoundException err){
+                        JOptionPane.showMessageDialog(null, "файл не найден");
+                        User a = new User(new Worker());
+                        a.DoCalculate();
                     }
-                    System.out.println((a[1].equals("+")) ? first + second : (a[1].equals("-")) ? first - second : (a[1].equals("*")) ? first * second : first / second);
-                } catch (NumberFormatException errorInt) {
-                    System.out.println("Error! Not number");
-                } catch (Exception b) {
-                    System.out.println(b.getMessage());
-                }
-            } catch (FileNotFoundException err) {
-                JOptionPane.showMessageDialog(null, "файл не найден");
-                User a = new User(new Worker());
-                a.DoCalculate();
+
+            }
+            catch (IOException ex){
+                JOptionPane.showMessageDialog(null, "IOException");
             }
         }
     }
